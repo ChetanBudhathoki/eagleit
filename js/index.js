8d1ba02d5ad09f914c69036fb0be8ec57ed4dd55
ChetanBudhathoki <chetanbudhathoki1028@gmail.com>
2022-11-30 21:56:13 +0545

    function integraged

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+// variables
 
+let position = -80;
 //selectors
-const menuBar=document.getElementsByClassName('menubar')
+const menuBar = document.querySelector(".menubar");
+const dropdownContainer = document.querySelector(".dropdown_container");
+console.log(menuBar);
+//eventlisteners
+
+menuBar.addEventListener("click", () => {
+  console.log("hwlloe");
+  // slidedrawer();
+
+  
+  while(position<=0){
+    setInterval(() => {
+        slidedrawer();
+      }, 10);
+  }
+});
+
+// functions
+
+const slidedrawer = () => {
+  console.log("hellow there");
+  dropdownContainer.style.left = position + "vw";
+  position=position+10;
+};
