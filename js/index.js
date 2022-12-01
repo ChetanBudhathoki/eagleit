96da03f990ae6b768398ba61107306200ed07234
ChetanBudhathoki <chetanbudhathoki1028@gmail.com>
2022-12-01 13:04:42 +0545

    menubar made functional and final touches given

--- js/index.js
+++ js/index.js
@@ -1,28 +1,44 @@
 // variables
-
 let position = -80;
+let isDrawerOpen = false;
+
 //selectors
 const menuBar = document.querySelector(".menubar");
 const dropdownContainer = document.querySelector(".dropdown_container");
-console.log(menuBar);
+
+//for icons
+const menuBtn = document.createElement("img")
+menuBtn.classList.add("menu_icon")
+menuBtn.src = "./assets/icons/bars-solid.svg"
+menuBar.appendChild(menuBtn);
 //eventlisteners
-
 menuBar.addEventListener("click", () => {
-  console.log("hwlloe");
-  // slidedrawer();
+  isDrawerOpen ? menuBtn.src = "./assets/icons/bars-solid.svg" : menuBtn.src = "./assets/icons/xmark-solid.svg";
 
-  
-  while(position<=0){
-    setInterval(() => {
-        slidedrawer();
-      }, 10);
-  }
+  isDrawerOpen = !isDrawerOpen;
+  console.log(isDrawerOpen);
+  let a = setInterval(() => {
+    slidedrawer();
+    dropdownContainer.style.left = position + "vw";
+    if (position >= 0 || position <= -80) {
+
+      clearInterval(a);
+      isDrawerOpen ? position = 0 : position = -80;
+    }
+  }, 5);
+
 });
 
 // functions
 
 const slidedrawer = () => {
-  console.log("hellow there");
-  dropdownContainer.style.left = position + "vw";
-  position=position+10;
+  if (isDrawerOpen) {
+    position += 1;
+  }
+  else {
+    position -= 1;
+  }
+  console.log(position);
 };
+
+
